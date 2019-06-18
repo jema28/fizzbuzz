c7f8ac1849e1d3a76b40f083944f4ff37194d8f7
Jem <jemila.abulhawa@gmail.com>
2019-06-18 14:26:52 +0300

    change back to tape is working

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var test = require('tape');
 var fizzbuzz = require('./index.js');
 
 test('Testing Tape is working', function(t) {
-  t.equal(fizzbuzz(), 1, 'One should equal one');
+  t.equal(1, 1, 'One should equal one');
   t.end();
 });
