fdcf63bdbd0d4fda87abbdd5f1ba8f7d164b39cd
Jem <jemila.abulhawa@gmail.com>
2019-06-18 13:59:33 +0300

    fix order - actual then expected

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var test = require('tape');
 var fizzbuzz = require('./index.js');
 
 test('Testing Tape is working', function(t) {
-  t.equal(1, fizzbuzz(), 'One should equal one');
+  t.equal(fizzbuzz(), 1, 'One should equal one');
   t.end();
 });
